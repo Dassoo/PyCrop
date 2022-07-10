29c6539a530077541109334d0499c1a515594f0d
Federico Dassiè <78917927+Dassoo@users.noreply.github.com>
2022-07-10 10:41:59 +0200

    Add files via upload

--- colorpicker.py
+++ colorpicker.py
@@ -1,18 +1,25 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog as fd
 from cv2 import cv2
 from PIL import Image
-from gooey import Gooey, GooeyParser
-import pyperclip
+import matplotlib.pyplot as plt
 import os
 
-# -- obsolete
-def pickerRGB(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:  # checks mouse moves
-        colorsBGR = image[y, x]
-        colorsRGB = tuple(reversed(colorsBGR)) #Reversing the OpenCV BGR format to RGB format
-        pyperclip.copy(colorsRGB)
-        print("RGB values copied to clipboard!")
-        print("RGB value at ({},{}):{} ".format(x,y,colorsRGB))
+counter = 0
+prev_point = []
 
+def click_data(event, x, y, flags, param):
+  global counter, prev_point
+  if (event == cv2.EVENT_LBUTTONDOWN):
+    print('\nSelected point: ',x,',',y)
+    if counter >= 1:   
+        print('> Difference from previous point: ',x-prev_point[0],',',y-prev_point[1])
+    prev_point = [x,y]
+    colorsBGR = image[y, x]
+    colorsRGB = tuple(reversed(colorsBGR)) #Reversing the OpenCV BGR format to RGB format
+    print("> RGB Value: {}".format(colorsRGB))
+    counter = counter + 1
 
 def conversion(path):
     size = len(path)
@@ -21,57 +28,29 @@ def conversion(path):
     rgb_im.save(path[:size-4]+'.jpg')
 
 
-# -- inizializzazione Gooey
-@Gooey(
-    program_name='Color Checker',       # Defaults to script name
-    default_size=(500, 350),   # starting size of the GUI
-    required_cols=1,           # number of columns in the "Required" section
-    optional_cols=1,           # number of columns in the "Optional" section
-    clear_before_run=True,
-    show_stop_warning=True,
-    show_failure_modal=True,
-    hide_progress_msg=True,
-    dump_build_config=False,   # Dump the JSON Gooey uses to configure itself
-    load_build_config=None,    # Loads a JSON Gooey-generated configuration
-    monospace_display=False,   # Uses a mono-spaced font in the output screen
-    #navigation="TABBED",
-    )   
-def main():
-    path = ''
-
-    parser = GooeyParser(description='Applicazione per estrazione colore')
-    subs = parser.add_subparsers(help="commands", dest="command")
-    # -- Required parameters section
-    fprocessing = subs.add_parser(
-        "aaa", prog="aaa",
-    ).add_argument_group("")
-
-    fprocessing.add_argument(
-        "inputImg",
-        metavar="Color checker",
-        help="Seleziona il file da esplorare, premi ESC per uscire dal visualizzatore (valori RGB visibili nella parte inferiore del viewer)",
-        widget="FileChooser"
-    )
-
-    args = vars(parser.parse_args())
-    path = args['inputImg']
+if __name__ == "__main__":
+    root = Tk()
+    root.wm_attributes('-alpha', 0)
+    filename = fd.askopenfilename()
+    path = filename
     size = len(path)
 
     conversion(path)
     image = cv2.imread(path[:size-4]+'.jpg')
- 
-    # Create a window and set Mousecallback to a function for that window
-    cv2.namedWindow('RGB Picker', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('RGB Picker', 900, 900) 
-    cv2.setMouseCallback('RGB Picker', pickerRGB)
-    # Do until esc pressed
-    while(1):
-        cv2.imshow('RGB Picker', image)
+
+    cv2.namedWindow('Image', cv2.WINDOW_GUI_EXPANDED)
+    cv2.resizeWindow('Image', 1200, 900)
+    cv2.imshow('Image', image)
+    cv2.setMouseCallback('Image', click_data)
+
+    while (1):
+        cv2.namedWindow('Image', cv2.WINDOW_GUI_EXPANDED)
+        cv2.resizeWindow('Image', 1200, 900)
+        cv2.imshow('Image', image)
         if cv2.waitKey(10) & 0xFF == 27:
             break
-    # if esc is pressed, close all windows.
+
     cv2.destroyAllWindows()
-    os.remove(path[:size-4]+'.jpg')
-
-
-main()
+        
+    if(path.endswith('.CR2')):
+        os.remove(path[:size-4]+'.jpg')
